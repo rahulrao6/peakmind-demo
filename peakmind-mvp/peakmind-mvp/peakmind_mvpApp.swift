962ebc8f6c1bed12d23a79bcad163b8e3a946792
Raj Jagirdar <jagirdar.r@northeastern.edu>
2024-02-12 22:14:31 -0500

    Original XCode Project create.

---
+++ peakmind-mvp/peakmind-mvp/peakmind_mvpApp.swift
@@ -0,0 +1,17 @@
+//
+//  peakmind_mvpApp.swift
+//  peakmind-mvp
+//
+//  Created by Raj Jagirdar on 2/12/24.
+//
+
+import SwiftUI
+
+@main
+struct peakmind_mvpApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

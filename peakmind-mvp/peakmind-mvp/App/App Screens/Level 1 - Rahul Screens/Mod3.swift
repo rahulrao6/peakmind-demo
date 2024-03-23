b48bb8a8924541deb510ffc126f7a562aa9690e0
rahulrao6 <117688680+rahulrao6@users.noreply.github.com>
2024-03-23 16:21:25 -0400

    adding new code for level 1 fixed

---
+++ peakmind-mvp/peakmind-mvp/App/App Screens/Level 1 - Rahul Screens/Mod3.swift
@@ -0,0 +1,18 @@
+//
+//  Mod3.swift
+//  peakmind-mvp
+//
+//  Created by Rahul Rao on 3/23/24.
+//
+
+import SwiftUI
+
+struct Mod3: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Mod3()
+}
---
+++ peakmind-mvp/peakmind-mvp/App/App Screens/Level 1 - Rahul Screens/Module1.swift
@@ -0,0 +1,18 @@
+//
+//  Module1.swift
+//  peakmind-mvp
+//
+//  Created by Rahul Rao on 3/20/24.
+//
+
+import SwiftUI
+
+struct Module1: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Module1()
+}

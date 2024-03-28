31fc0e6922ef434fa0c2b9260b51665ee744e1c6
James <james@jamesssystems.info>
2024-03-28 07:47:03 -0700

    Add working breathing exercise
    this took awhile but its cool

--- peakmind-mvp/peakmind-mvp/Game Level Screens/Level 3/James/BreathingExerciseView.swift
+++ peakmind-mvp/peakmind-mvp/Game Level Screens/Level 3/James/BreathingExerciseView.swift
@@ -6,12 +6,44 @@
 //
 
 import SwiftUI
+import AVFoundation
+import AVKit
+import SpriteKit
+
+//
+final class SpriteKitScene: SKScene {
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override init(size: CGSize) {
+        super.init(size: size)
+        let player = AVPlayer(url: Bundle.main.url(forResource: "pkmdTriangleEncoded", withExtension: "mov")!)
+        NotificationCenter.default.addObserver(forName: NSNotification.Name.AVPlayerItemDidPlayToEndTime, object: nil, queue: nil) { notification in
+            player.seek(to: CMTime.zero)
+            player.play()
+        }
+        // make sure aspect ratio is right
+        if let track = player.currentItem?.asset.tracks(withMediaType: .video).first {
+            let videoSize = track.naturalSize.applying(track.preferredTransform)
+            let videoAspectRatio = abs(videoSize.width / videoSize.height)
+
+            let video = SKVideoNode(avPlayer: player)
+            video.size = CGSize(width: size.width, height: size.width / videoAspectRatio)
+            video.position = CGPoint(x: self.size.width / 2, y: self.size.height / 2)
+            addChild(video)
+            player.play()
+            
+            scaleMode = .aspectFill
+            backgroundColor = .clear
+        } else {
+            fatalError("Could not retrieve video tracks")
+        }
+    }
+}
+
 
 struct BreathingExerciseView: View {
-    @EnvironmentObject var viewModel: AuthViewModel
-    @State private var userAnswer: String = ""
-    @State private var showThankYou = false
-
     var body: some View {
         ZStack {
             // Background
@@ -19,7 +51,7 @@ struct BreathingExerciseView: View {
                 .resizable()
                 .scaledToFill()
                 .edgesIgnoringSafeArea(.all)
-
+            
             // Content
             VStack {
                 // Title
@@ -27,7 +59,8 @@ struct BreathingExerciseView: View {
                     .font(.system(size: 30, weight: .bold, design: .default))
                     .foregroundColor(.white)
                     .padding(.bottom, 40)
-
+                
+                // First textbox
                 VStack {
                     Text("Breathing for Stress")
                         .font(.system(size: 28, weight: .bold, design: .default))
@@ -46,22 +79,27 @@ struct BreathingExerciseView: View {
                 .cornerRadius(30)
                 .shadow(radius: 5)
                 
-                Image("breathingTriangle")
-                    .resizable()
-                    .scaledToFit()
-                    .frame(width: 220)
+                // SpriteKit implementation for the transparent video
+                SpriteKitUIView(scene: SpriteKitScene(size: CGSize(width: 200, height: 200)))
+                    .frame(width: 250, height: 200)
+                    .background(Color.clear)
                 
-                
-                
-
                 Spacer()
-
+                
+                // Instruction Text
+                
+                Text("Tap to Continue")
+                    .foregroundColor(.white)
+                    .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .bottomTrailing)
+                    .padding()
+                    .offset(x: -120, y: 200)
+                
                 // Sherpa Image and Prompt
                 HStack {
                     Image("Sherpa")
                         .resizable()
                         .scaledToFit()
-                        .frame(width: 120)
+                        .frame(width: 120, height: 150)
                     
                     Text("Lets learn a breathing method!")
                         .font(.headline)

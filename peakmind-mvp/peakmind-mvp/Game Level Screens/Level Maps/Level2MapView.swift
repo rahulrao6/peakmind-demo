2331bc2edab6dfa25ec3a41a23dc09f2a4fda87f
Mikey Halim <kechio.halim@gmail.com>
2024-04-26 12:55:24 -0400

    changes

---
+++ peakmind-mvp/peakmind-mvp/Game Level Screens/Level Maps/Level2MapView.swift
@@ -0,0 +1,18 @@
+//
+//  Level2MapView.swift
+//  peakmind-mvp
+//
+//  Created by Mikey Halim on 4/26/24.
+//
+
+import SwiftUI
+
+struct Level2MapView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Level2MapView()
+}

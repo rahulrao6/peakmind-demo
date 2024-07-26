40e0855aa4e4f421066c618e7b3165c528ef62fe
Mikey Halim <kechio.halim@gmail.com>
2024-07-26 12:28:55 -0700

    self care suite update

---
+++ peakmind-mvp/peakmind-mvp/Core Screens/Check In/CustomWidgetView.swift
@@ -0,0 +1,18 @@
+//
+//  CustomWidgetView.swift
+//  peakmind-mvp
+//
+//  Created by Mikey Halim on 7/24/24.
+//
+
+import SwiftUI
+
+struct CustomWidgetView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CustomWidgetView()
+}
---
+++ peakmind-mvp/peakmind-mvp/Core Screens/Self Care Suite/SelfCareNew.swift
@@ -0,0 +1,18 @@
+//
+//  SelfCareNew.swift
+//  peakmind-mvp
+//
+//  Created by Mikey Halim on 7/25/24.
+//
+
+import SwiftUI
+
+struct SelfCareNew: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SelfCareNew()
+}

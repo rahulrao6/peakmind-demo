e1f98ea07a862f167bff059fb964f85a59bf6746
Mikey Halim <kechio.halim@gmail.com>
2024-07-26 12:29:19 -0700

    self care suite

--- peakmind-mvp/peakmind-mvp/Core Screens/Check In/CustomWidgetView.swift
+++ peakmind-mvp/peakmind-mvp/Core Screens/Check In/CustomWidgetView.swift
@@ -1,15 +1,273 @@
-//
-//  CustomWidgetView.swift
-//  peakmind-mvp
-//
-//  Created by Mikey Halim on 7/24/24.
-//
-
 import SwiftUI
 
 struct CustomWidgetView: View {
+    @State private var widgets: [CustomWidget] = [
+        CustomWidget(name: "Steps", frequency: .daily, unit: .count, specificUnit: "steps", value: 1000, description: "Daily steps count"),
+        CustomWidget(name: "Water", frequency: .multipleTimesDaily, unit: .volume, specificUnit: "liters", value: 2, description: "Daily water intake"),
+        CustomWidget(name: "Reading", frequency: .daily, unit: .time, specificUnit: "minutes", value: 30, description: "Daily reading time")
+    ]
+    @State private var showingAddWidget = false
+    @State private var editingWidget: CustomWidget? = nil
+    @State private var isEditMode = false
+
+    let cardWidth: CGFloat = 200
+    let cardHeight: CGFloat = 250
+    let cardSpacing: CGFloat = -70
+
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationView {
+            VStack {
+                ScrollView(.horizontal, showsIndicators: false) {
+                    HStack(spacing: cardSpacing) {
+                        ForEach(widgets.indices, id: \.self) { index in
+                            WidgetView2(widget: $widgets[index], isEditMode: $isEditMode)
+                                .frame(width: cardWidth, height: cardHeight)
+                                .onTapGesture {
+                                    if isEditMode {
+                                        editingWidget = widgets[index]
+                                    }
+                                }
+                        }
+                        AddWidgetButton {
+                            showingAddWidget = true
+                        }
+                        .frame(width: cardWidth, height: cardHeight) // Ensure the same size as other cards
+                    }
+                    .padding(.horizontal)
+                }
+                .padding(.top)
+                Spacer()
+            }
+            .background(Color.sentMessage.edgesIgnoringSafeArea(.all)) // Set background color
+            .navigationTitle("Your Widgets")
+            .navigationBarTitleDisplayMode(.inline) // Make sure the title is in-line
+            .toolbar {
+                ToolbarItem(placement: .principal) {
+                    Text("Your Widgets")
+                        .foregroundColor(.white) // Set the color to white
+                        .font(.headline)
+                }
+                ToolbarItem(placement: .navigationBarTrailing) {
+                    Button(isEditMode ? "Done" : "Edit Widgets") {
+                        isEditMode.toggle()
+                    }
+                }
+            }
+            .onAppear {
+                let appearance = UINavigationBarAppearance()
+                appearance.configureWithOpaqueBackground()
+                appearance.backgroundColor = UIColor(Color.sentMessage) // Use the custom background color
+                appearance.titleTextAttributes = [.foregroundColor: UIColor.white]
+                appearance.largeTitleTextAttributes = [.foregroundColor: UIColor.white]
+
+                UINavigationBar.appearance().standardAppearance = appearance
+                UINavigationBar.appearance().scrollEdgeAppearance = appearance
+            }
+            .sheet(isPresented: $showingAddWidget) {
+                AddWidgetView(widgets: $widgets)
+            }
+            .sheet(item: $editingWidget) { widget in
+                AddWidgetView(widgets: $widgets, widgetToEdit: widget)
+            }
+        }
+    }
+}
+
+struct CustomWidget: Identifiable, Equatable {
+    let id = UUID()
+    var name: String
+    var frequency: TrackingFrequency
+    var unit: MeasurementUnit
+    var specificUnit: String
+    var value: Int = 0
+    var description: String = ""
+    var percentageChange: Int = 0
+
+}
+
+enum TrackingFrequency: String, CaseIterable {
+    case daily = "Daily"
+    case multipleTimesDaily = "Multiple times a day"
+}
+
+enum MeasurementUnit: String, CaseIterable {
+    case length = "Length"
+    case weight = "Weight"
+    case time = "Time"
+    case volume = "Volume"
+    case count = "Count"
+
+    var specificUnits: [String] {
+        switch self {
+        case .length: return ["meters", "feet", "inches"]
+        case .weight: return ["kg", "lbs"]
+        case .time: return ["seconds", "minutes", "hours"]
+        case .volume: return ["liters", "milliliters", "gallons"]
+        case .count: return ["items", "steps"]
+        }
+    }
+}
+
+struct WidgetView2: View {
+    @Binding var widget: CustomWidget
+    @Binding var isEditMode: Bool
+
+    var body: some View {
+        VStack(spacing: 5) {
+            Text(widget.name)
+                .font(.headline)
+                .foregroundColor(isEditMode ? .blue : .primary)
+                .lineLimit(1)
+                .truncationMode(.tail)
+            Text("\(widget.value)")
+                .font(.title2)
+                .foregroundColor(.white)
+            Text(widget.specificUnit)
+                .font(.subheadline)
+                .foregroundColor(.white)
+                .padding(.top, -5)
+            
+            HStack(spacing: 15) {
+                Button(action: {
+                    widget.value += 1
+                }) {
+                    Image(systemName: "plus.circle.fill")
+                        .font(.title)
+                        .foregroundColor(.iceBlue)
+                }
+                Button(action: {
+                    if widget.value > 0 {
+                        widget.value -= 1
+                    }
+                }) {
+                    Image(systemName: "minus.circle.fill")
+                        .font(.title)
+                        .foregroundColor(.iceBlue)
+                }
+            }
+            .padding(.top, 10)
+        }
+        .frame(height: 130)
+        .frame(width: 80)
+        .padding()
+        .background(isEditMode ? Color.blue.opacity(0.2) : Color.mediumBlue.opacity(0.7))
+        .cornerRadius(10)
+        .overlay(
+            RoundedRectangle(cornerRadius: 10)
+                .stroke(isEditMode ? Color.blue : Color.clear, lineWidth: 2)
+        )
+        .shadow(color: Color.black.opacity(0.2), radius: 5, x: 0, y: 5) // Added shadow properties
+    }
+}
+
+struct AddWidgetButton: View {
+    var action: () -> Void
+
+    var body: some View {
+        Button(action: action) {
+            VStack {
+                Spacer() // This ensures the text and image are centered vertically
+                Image(systemName: "plus.circle.fill")
+                    .font(.largeTitle)
+                    .foregroundColor(.black) // Set plus icon to black
+                Text("Add Widget")
+                    .font(.headline)
+                Spacer() // This ensures the text and image are centered vertically
+            }
+            .frame(height: 130) // Fixed height for content within the card
+            .frame(width: 80) // Fixed height for content within the card
+            .padding()
+            .background(Color.iceBlue.opacity(1.0)) // Use the custom "Ice Blue" color
+            .cornerRadius(10)
+            .frame(width: 200, height: 250) // Ensure the same height as other cards
+        }
+    }
+}
+
+struct AddWidgetView: View {
+    @Environment(\.presentationMode) var presentationMode
+    @Binding var widgets: [CustomWidget]
+    var widgetToEdit: CustomWidget? = nil
+
+    @State private var name = ""
+    @State private var frequency: TrackingFrequency = .daily
+    @State private var unit: MeasurementUnit = .length
+    @State private var specificUnit = ""
+    @State private var description = ""
+    @State private var showError = false
+
+    var body: some View {
+        NavigationView {
+            Form {
+                Section(header: Text("Widget Name")) {
+                    TextField("Name", text: $name)
+                        .onChange(of: name) { newValue in
+                            if newValue.count > 10 {
+                                name = String(newValue.prefix(10))
+                            }
+                        }
+                        .foregroundColor(name.count > 10 ? .red : .primary)
+                }
+                Section(header: Text("Frequency")) {
+                    Picker("Frequency", selection: $frequency) {
+                        ForEach(TrackingFrequency.allCases, id: \.self) {
+                            Text($0.rawValue)
+                        }
+                    }
+                    .pickerStyle(SegmentedPickerStyle())
+                }
+                Section(header: Text("Unit")) {
+                    Picker("Unit", selection: $unit) {
+                        ForEach(MeasurementUnit.allCases, id: \.self) {
+                            Text($0.rawValue)
+                        }
+                    }
+                    .pickerStyle(SegmentedPickerStyle())
+                    
+                    if !unit.specificUnits.isEmpty {
+                        Picker("Specific Unit", selection: $specificUnit) {
+                            ForEach(unit.specificUnits, id: \.self) {
+                                Text($0)
+                            }
+                        }
+                        .pickerStyle(SegmentedPickerStyle())
+                    }
+                }
+                Section(header: Text("Description")) {
+                    TextField("Description", text: $description)
+                }
+            }
+            .navigationTitle(widgetToEdit == nil ? "New Widget" : "Edit Widget")
+            .navigationBarItems(trailing: Button("Save") {
+                if name.isEmpty || specificUnit.isEmpty {
+                    showError = true
+                } else {
+                    if let widgetToEdit = widgetToEdit, let index = widgets.firstIndex(where: { $0.id == widgetToEdit.id }) {
+                        widgets[index].name = name
+                        widgets[index].frequency = frequency
+                        widgets[index].unit = unit
+                        widgets[index].specificUnit = specificUnit
+                        widgets[index].description = description
+                    } else {
+                        let newWidget = CustomWidget(name: name, frequency: frequency, unit: unit, specificUnit: specificUnit, description: description)
+                        widgets.append(newWidget)
+                    }
+                    presentationMode.wrappedValue.dismiss()
+                }
+            })
+            .alert(isPresented: $showError) {
+                Alert(title: Text("Error"), message: Text("Please fill in all required fields."), dismissButton: .default(Text("OK")))
+            }
+            .onAppear {
+                if let widgetToEdit = widgetToEdit {
+                    name = widgetToEdit.name
+                    frequency = widgetToEdit.frequency
+                    unit = widgetToEdit.unit
+                    specificUnit = widgetToEdit.specificUnit
+                    description = widgetToEdit.description
+                }
+            }
+        }
     }
 }
 
--- peakmind-mvp/peakmind-mvp/Core Screens/Self Care Suite/SelfCareNew.swift
+++ peakmind-mvp/peakmind-mvp/Core Screens/Self Care Suite/SelfCareNew.swift
@@ -1,15 +1,215 @@
-//
-//  SelfCareNew.swift
-//  peakmind-mvp
-//
-//  Created by Mikey Halim on 7/25/24.
-//
-
 import SwiftUI
 
 struct SelfCareNew: View {
+    @State private var showingAddWidget = false
+    @State private var editingWidget: CustomWidget? = nil
+    @State private var isEditMode = false
+    @State private var selectedWidget: CustomWidget?
+
+    @State private var widgets: [CustomWidget] = [
+        CustomWidget(name: "Steps", frequency: .daily, unit: .count, specificUnit: "steps", value: 1000, description: "Daily steps count", percentageChange: 5),
+        CustomWidget(name: "Water", frequency: .multipleTimesDaily, unit: .volume, specificUnit: "liters", value: 2, description: "Daily water intake", percentageChange: -3),
+        CustomWidget(name: "Reading", frequency: .daily, unit: .time, specificUnit: "minutes", value: 30, description: "Daily reading time", percentageChange: 8),
+        CustomWidget(name: "Exercise", frequency: .daily, unit: .time, specificUnit: "minutes", value: 60, description: "Daily exercise time", percentageChange: 10),
+        CustomWidget(name: "Meditation", frequency: .daily, unit: .time, specificUnit: "minutes", value: 15, description: "Daily meditation time", percentageChange: 2),
+        CustomWidget(name: "Sleep", frequency: .daily, unit: .time, specificUnit: "hours", value: 8, description: "Daily sleep duration", percentageChange: 1)
+    ]
+    
+    let cardWidth: CGFloat = 200
+    let cardHeight: CGFloat = 250
+    let cardSpacing: CGFloat = -70
+    let defaultUsername: String = "User" // Default username for preview
+
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationView {
+            VStack(spacing: 0) {
+                // Top Section
+                ZStack {
+                    Image("Header")
+                        .resizable()
+                        .aspectRatio(contentMode: .fill)
+                        .frame(height: 100)
+                        .edgesIgnoringSafeArea(.top)
+                    
+                    HStack {
+                        VStack(alignment: .leading) {
+                            Text("Welcome, \(defaultUsername)!")
+                                .fontWeight(.bold)
+                                .foregroundColor(.black)
+                                .font(.title)
+                            
+                            Text("Check out your analytics below.")
+                                .fontWeight(.semibold)
+                                .foregroundColor(.black)
+                                .font(.body)
+                        }
+                        .padding()
+                        
+                        Spacer()
+                        
+                        Image("Sherpa")
+                            .resizable()
+                            .scaledToFit()
+                            .frame(width: 80, height: 80)
+                            .padding()
+                    }
+                    .padding(.top, 50) // Adjust top padding to position the text and image
+                }
+                .frame(height: 70)
+
+                CustomButton2(title: "Daily Check In", onClick: {
+                    Task{
+                    }
+                })
+                .padding(.top, 60)
+                .padding(.bottom, -41)
+                
+                // Widgets Section
+                ScrollView(.horizontal, showsIndicators: false) {
+                    HStack(spacing: cardSpacing) {
+                        ForEach(widgets.indices, id: \.self) { index in
+                            WidgetView2(widget: $widgets[index], isEditMode: $isEditMode)
+                                .frame(width: cardWidth, height: cardHeight)
+                                .onTapGesture {
+                                    if isEditMode {
+                                        editingWidget = widgets[index]
+                                    }
+                                }
+                        }
+                        AddWidgetButton {
+                            showingAddWidget = true
+                        }
+                        .frame(width: cardWidth, height: cardHeight)
+                    }
+                    .padding(.horizontal)
+                }
+                .padding(.top)
+                .padding(.leading, -48)
+
+                // Edit Widgets Button
+                CustomButton3(title: isEditMode ? "Done" : "Edit Widgets", onClick: {
+                    isEditMode.toggle()
+                })
+                .frame(width: 390) // Smaller width for the button
+                .padding(.top, -30)
+                // Swipeable Graph Interface
+                GeometryReader { geometry in
+                    TabView {
+                        ForEach(0..<((widgets.count + 1) / 2), id: \.self) { index in
+                            HStack(spacing: -1) { // Set the spacing to 0 or any value you prefer
+                                ForEach(0..<2) { subIndex in
+                                    if index * 2 + subIndex < widgets.count {
+                                        GraphCardView(widget: widgets[index * 2 + subIndex])
+                                            .frame(width: geometry.size.width / 2)
+                                            .onTapGesture {
+                                                selectedWidget = widgets[index * 2 + subIndex]
+                                            }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                    .tabViewStyle(PageTabViewStyle(indexDisplayMode: .never)) // Remove page indicators
+                }
+                .frame(height: 120) // Set height for the graph cards
+                .padding(.bottom)
+                CustomButton2(title: "Detailed Analytics", onClick: {
+                    Task{
+                    }
+                })
+                .padding(.top, -14)
+                .padding(.bottom, -41)
+                Spacer()
+            }
+            .background(Color.sentMessage.edgesIgnoringSafeArea(.all))
+            .navigationBarHidden(true)
+            .sheet(isPresented: $showingAddWidget) {
+                AddWidgetView(widgets: $widgets)
+            }
+            .sheet(item: $editingWidget) { widget in
+                AddWidgetView(widgets: $widgets, widgetToEdit: widget)
+            }
+            .sheet(item: $selectedWidget) { widget in
+                WidgetDetailView(widget: widget)
+            }
+        }
+    }
+}
+
+import SwiftUI
+
+struct GraphCardView: View {
+    var widget: CustomWidget
+    let fixedWidth: CGFloat = 165 // Set your desired fixed width here
+
+    var body: some View {
+        VStack(alignment: .leading, spacing: 10) {
+            Text(widget.name)
+                .font(.headline)
+                .padding(.top, 5)
+            
+            HStack {
+                Text("\(widget.percentageChange >= 0 ? "+" : "")\(widget.percentageChange)%")
+                    .foregroundColor(widget.percentageChange >= 0 ? .green : .red)
+                    .font(.subheadline)
+                
+                TrendLineView(trend: widget.percentageChange >= 0 ? .up : .down)
+                    .frame(width: 30, height: 30)
+            }
+            .padding(.vertical, 5)
+        }
+        .padding(.horizontal, 20)
+        .frame(width: fixedWidth)
+        .background(Color.white)
+        .cornerRadius(10)
+        .padding(.horizontal, 10)
+        .shadow(color: Color.black.opacity(0.2), radius: 5, x: 0, y: 5) // Added shadow properties
+    }
+}
+
+
+
+enum Trend {
+    case up, down, neutral
+}
+
+struct TrendLineView: View {
+    var trend: Trend
+
+    var body: some View {
+        GeometryReader { geometry in
+            Path { path in
+                let width = geometry.size.width
+                let height = geometry.size.height
+                let startY: CGFloat = trend == .down ? 0 : trend == .up ? height : height / 2
+                let endY: CGFloat = trend == .down ? height : trend == .up ? 0 : height / 2
+
+                path.move(to: CGPoint(x: 0, y: startY))
+                path.addLine(to: CGPoint(x: width, y: endY))
+            }
+            .stroke(trend == .down ? Color.red : trend == .up ? Color.green : Color.gray, lineWidth: 2)
+        }
+    }
+}
+
+struct WidgetDetailView: View {
+    var widget: CustomWidget
+
+    var body: some View {
+        VStack {
+            Text(widget.name)
+                .font(.largeTitle)
+                .padding()
+
+            Text("Details about the widget will be displayed here.")
+                .padding()
+
+            Spacer()
+        }
+        .background(Color.white)
+        .cornerRadius(10)
+        .shadow(radius: 5)
+        .padding()
     }
 }
 

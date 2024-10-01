df06dbffab2a0fc33308e8606d43c9df74eb72d2
James <james@jamesssystems.info>
2024-09-30 20:52:54 -0700

    add styling to coming soon pages

--- peakmind-mvp/Redesigns/GameUnavailable.swift
+++ peakmind-mvp/Redesigns/GameUnavailable.swift
@@ -11,9 +11,50 @@ struct GameUnavailable: View {
     var closeAction: () -> Void
     
     var body: some View {
-        Text("This minigame is coming soon!")
-        Button("Close") {
-            closeAction()
+        VStack(spacing: 16) {
+            Spacer()
+                .frame(height: 120)
+            
+            // Coming Soon Text
+            Text("This minigame is coming soon!")
+                .font(.custom("SFProText-Bold", size: 30))
+                .foregroundColor(Color("QuestionHeaderColor"))
+                .multilineTextAlignment(.center)
+                .padding(.horizontal, 20)
+                .shadow(color: Color.white.opacity(0.3), radius: 5, x: 0, y: 0)
+            
+            Spacer()
+            
+            // Next Button
+            Button(action: {
+                closeAction()
+            }) {
+                Text("Next")
+                    .font(.custom("SFProText-Bold", size: 20))
+                    .foregroundColor(.white)
+                    .padding(.vertical, 8)
+                    .padding(.horizontal, 12)
+                    .background(
+                        LinearGradient(
+                            gradient: Gradient(colors: [Color("ButtonGradient1"), Color("ButtonGradient2")]),
+                            startPoint: .topLeading,
+                            endPoint: .bottomTrailing
+                        )
+                    )
+                    .cornerRadius(15)
+                    .shadow(color: Color.white.opacity(1), radius: 10, x: 0, y: 0)
+            }
+            .padding(.bottom, 50)
         }
+        .padding(.horizontal)
+        .background(
+            Image("NewBG")
+                .resizable()
+                .edgesIgnoringSafeArea(.all)
+        )
     }
+}
+
+#Preview {
+    GameUnavailable(closeAction: {print("CLOSE")})
 }

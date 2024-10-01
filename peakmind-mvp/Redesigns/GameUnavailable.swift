d068ddf3e4fb81c4d984f6c498e8b0c7d28ae500
Mikey Halim <kechio.halim@gmail.com>
2024-09-30 21:16:00 -0700

    james code merge

---
+++ peakmind-mvp/Redesigns/GameUnavailable.swift
@@ -0,0 +1,18 @@
+//
+//  GameAvailable.swift
+//  peakmind-mvp
+//
+//  Created by Mikey Halim on 9/30/24.
+//
+
+import SwiftUI
+
+struct GameAvailable: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    GameAvailable()
+}
---
+++ peakmind-mvp/Redesigns/LevelUnavailable.swift
@@ -0,0 +1,18 @@
+//
+//  LevelUnavailable.swift
+//  peakmind-mvp
+//
+//  Created by Mikey Halim on 9/30/24.
+//
+
+import SwiftUI
+
+struct LevelUnavailable: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LevelUnavailable()
+}

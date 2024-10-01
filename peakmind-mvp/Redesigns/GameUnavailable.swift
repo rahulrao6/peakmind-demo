3da2eb4af8dd1f569a7ccfc435f5b5d92e79b7db
James <james@jamesssystems.info>
2024-09-30 20:44:16 -0700

    Fix level names, phase animation bug, and progression issue

---
+++ peakmind-mvp/Redesigns/GameUnavailable.swift
@@ -0,0 +1,23 @@
+//
+//  GameUnavailable.swift
+//  peakmind-mvp
+//
+//  Created by James Wilson on 9/30/24.
+//
+
+import SwiftUI
+
+struct GameUnavailable: View {
+    var closeAction: () -> Void
+    
+    var body: some View {
+        Text("This minigame is coming soon!")
+        Button("Close") {
+            closeAction()
+        }
+    }
+}
+
+#Preview {
+    GameUnavailable()
+}

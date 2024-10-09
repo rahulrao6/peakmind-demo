ebf17e25f69328e14fb492575019e6e0413d3c61
Mikey Halim <kechio.halim@gmail.com>
2024-10-08 18:05:30 -0700

    bug fixes

--- peakmind-mvp/Redesigns/Phase1GameReady/PurpleNewBG1_GameReady.swift
+++ peakmind-mvp/Redesigns/Phase1GameReady/PurpleNewBG1_GameReady.swift
@@ -1,10 +1,3 @@
-//
-//  PurpleNewBG1.swift
-//  peakmind-mvp
-//
-//  Created by ZA on 8/22/24.
-//
-
 import SwiftUI
 
 struct P1_1: View {
@@ -12,55 +5,58 @@ struct P1_1: View {
     var closeAction: (String) -> Void
     
     var body: some View {
-        VStack(spacing: 16) {
-            Spacer()
-                .frame(height: 160)
-            
-            // Intro Text
-            Text("Let's explore an educational module designed to delve into various aspects of mental health, equipping you with coping strategies along the way.")
-                .font(.custom("SFProText-Bold", size: 30))
-                .foregroundColor(Color("PurpleTitleColor"))
-                .multilineTextAlignment(.center)
-                .padding(.horizontal, 20)
-                .shadow(color: Color.white.opacity(0.3), radius: 5, x: 0, y: 0)
-            
-            Spacer()
-            
-            // Next Button
-            Button(action: {
-                showNextScreen = true
-                closeAction("You completed your first level!")
-                
-            }) {
-                Text("Next")
-                    .font(.custom("SFProText-Bold", size: 20))
-                    .foregroundColor(.white)
-                    .padding(.vertical, 8)
-                    .padding(.horizontal, 12)
-                    .background(
-                        LinearGradient(
-                            gradient: Gradient(colors: [Color("PurpleButtonGradientColor1"), Color("PurpleButtonGradientColor2")]),
-                            startPoint: .topLeading,
-                            endPoint: .bottomTrailing
-                        )
-                    )
-                    .cornerRadius(15)
-                    .shadow(color: Color.white.opacity(1), radius: 10, x: 0, y: 0)
-            }
-            .padding(.bottom, 50)
-            .background(
-                NavigationLink(
-                    destination: MentalHealthFeatureView(),
-                    isActive: $showNextScreen,
-                    label: { EmptyView() }
-                )
-            )
-        }
-        .padding(.horizontal)
-        .background(
+        ZStack {
+            // Background image
             Image("PurpleNewBG")
                 .resizable()
-                .edgesIgnoringSafeArea(.all)
-        )
+                .ignoresSafeArea(.all) // Ensure the image covers the whole screen including the sides and vertical space
+            
+            // Content
+            VStack(spacing: 16) {
+                Spacer()
+                    .frame(height: 160)
+                
+                // Intro Text
+                Text("Let's explore an educational module designed to delve into various aspects of mental health, equipping you with coping strategies along the way.")
+                    .font(.custom("SFProText-Bold", size: 30))
+                    .foregroundColor(Color("PurpleTitleColor"))
+                    .multilineTextAlignment(.center)
+                    .padding(.horizontal, 20)
+                    .shadow(color: Color.white.opacity(0.3), radius: 5, x: 0, y: 0)
+                
+                Spacer()
+                
+                // Next Button
+                Button(action: {
+                    showNextScreen = true
+                    closeAction("You completed your first level!")
+                    
+                }) {
+                    Text("Next")
+                        .font(.custom("SFProText-Bold", size: 20))
+                        .foregroundColor(.white)
+                        .padding(.vertical, 8)
+                        .padding(.horizontal, 12)
+                        .background(
+                            LinearGradient(
+                                gradient: Gradient(colors: [Color("PurpleButtonGradientColor1"), Color("PurpleButtonGradientColor2")]),
+                                startPoint: .topLeading,
+                                endPoint: .bottomTrailing
+                            )
+                        )
+                        .cornerRadius(15)
+                        .shadow(color: Color.white.opacity(1), radius: 10, x: 0, y: 0)
+                }
+                .padding(.bottom, 50)
+                .background(
+                    NavigationLink(
+                        destination: MentalHealthFeatureView(),
+                        isActive: $showNextScreen,
+                        label: { EmptyView() }
+                    )
+                )
+            }
+            .padding(.horizontal)
+        }
     }
 }
